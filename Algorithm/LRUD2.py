0678a17e5b3c6e19a2d8cfda77a54df5eff3bcbb
kimasill <80378085+kimasill@users.noreply.github.com>
2023-10-19 04:24:53 +0000

    algorithm update
    LRUD

--- Algorithm/LRUD2.py
+++ Algorithm/LRUD2.py
@@ -1,14 +1,20 @@
-class LRUD2:
+print("상하좌우 알고리즘2")
+x,y = 1,1
+plan = input().split()
+size = input()
 
-  def StartAlgorithm(self):
-    print("상하좌우 알고리즘2")
-    x,y = 1,1
-    plan = input().split()
-    size = input()
-
-    plans = ["L", "R", "U", "D"]
-
-    for i in plan:
-      for i in range(len(plan)):
-      
+xpos =[-1, 1, 0, 0]
+ypos =[0, 0, -1, 1]
+plans = ["L", "R", "U", "D"]
+tx, ty = x, y
+for c in plan:
+  for i in range(len(plans)):
+    if c == plans[i] :
+      tx += xpos[i]
+      ty += ypos[i]
+      if tx < 1 or tx > int(size) or ty < 1 or ty > int(size):
+        print("Out of Range")
         continue
+      x+= xpos[i]
+      y+= ypos[i]
+print(y,x)
